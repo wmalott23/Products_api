29594e3593cd2b79994be0d4ffd4c3e5864b4707
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-03-31 09:43:24 -0700

    Added blank=true in image url

--- products/models.py
+++ products/models.py
@@ -8,4 +8,4 @@ class Product(models.Model):
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory_quantity = models.IntegerField()
-    image_url = models.CharField(max_length=255, null=True)
+    image_url = models.CharField(max_length=255, null=True, blank=True)
