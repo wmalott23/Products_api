d60696c77b43a4a0f36a7761fa1680f5d66c1a00
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-03-31 08:06:21 -0700

    Added rest framework and products to installed apps in settings + migrated new model to database

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
+from unicodedata import decimal
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory_quanitity = models.IntegerField()
