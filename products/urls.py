89cebe6f1eee22621212da704e520fe86408c91f
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-03-31 08:34:42 -0700

    Added int:pk to urls

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.products_list)
+    path('', views.products_list),
+    path('int:pk', views.products_list),
 ]
--- products/views.py
+++ products/views.py
@@ -1,10 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import ProductSerializer
+from .models import Product
 
 # Create your views here.
 
 @api_view(['GET'])
 def products_list(request):
-    
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
 
-    return Response('ok')
+    return Response(serializer.data)
