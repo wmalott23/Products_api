a33fa1d5f6fddb6d3477d771ffbe73549fb27318
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-03-31 07:43:37 -0700

    Moved some files around to their correct location + created local settings file + moved security key and database info to local settings file

--- products_project/settings.py
+++ products_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wj%-ia&bl6_5dlj84my@twiptel0_2ti119)nuisrxm+a(n_6a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,15 +68,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'products_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
