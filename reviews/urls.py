516aa180a98ee3536c0a5fc8a56065b3c8177232
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-04-01 09:37:43 -0700

    Adjusted review to be class based

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('', views.reviews_list),
-    path('<int:pk>/', views.reviews_detail),
+    # path('', views.reviews_list),
+    # path('<int:pk>/', views.reviews_detail),
+    path('', views.ReviewList.as_view()),
+    path('<int:pk>/', views.ReviewDetail.as_view()),
     path('product/<int:pk>/', views.product_reviews)
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- reviews/views.py
+++ reviews/views.py
@@ -5,35 +5,46 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializer import ReviewSerializer
 from .models import Review
+from rest_framework import generics
+
+
+
+class ReviewList(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
 
 # Create your views here.
 
-@api_view(['GET', 'POST'])
-def reviews_list(request):
-    if request.method == 'GET':
-        reviews = Review.objects.all()
-        serializer = ReviewSerializer(reviews, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = ReviewSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+# @api_view(['GET', 'POST'])
+# def reviews_list(request):
+#     if request.method == 'GET':
+#         reviews = Review.objects.all()
+#         serializer = ReviewSerializer(reviews, many=True)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = ReviewSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def reviews_detail(request, pk):
-    review = get_object_or_404(Review, pk=pk)
-    if request.method == 'GET':
-        serializer = ReviewSerializer(review)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = ReviewSerializer(review, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    elif request.method == 'DELETE':
-        review.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def reviews_detail(request, pk):
+#     review = get_object_or_404(Review, pk=pk)
+#     if request.method == 'GET':
+#         serializer = ReviewSerializer(review)
+#         return Response(serializer.data)
+#     elif request.method == 'PUT':
+#         serializer = ReviewSerializer(review, data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+#     elif request.method == 'DELETE':
+#         review.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET'])
 def product_reviews(request, pk):
